d4bb14bbb2e6c3b57f54558e109193e0150998ad
KeyJay86 <keyjay1@gmail.com>
2014-12-21 11:47:49 +0100

    test
    test

--- animation_test_HW01SW02/selftest.ino
+++ animation_test_HW01SW02/selftest.ino
@@ -1,11 +1,11 @@
-// this function will switch every LED short on and off
+
 
 void selftest(int pause){                                                      
   // Move a single white led 
-  for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+  for(int whiteLed = 0; whiteLed < WS2812BCOUNT; whiteLed = whiteLed + 1) {
     // Turn our current led on to white, then show the leds
     leds[whiteLed] = CRGB::White;
-    FastLED.setBrightness(brightness);
+    FastLED.setBrightness(BRIGHTNESS);
     // Show the leds (only one of which is set to white, from above)
     FastLED.show();
 
@@ -15,9 +15,39 @@ void selftest(int pause){
     // Turn our current led back to black for the next loop around
     leds[whiteLed] = CRGB::Black;
   }
-     writeChar('H');
-     writeChar('A');
-     writeChar('L');
-     writeChar('L');
-     writeChar('O');
+   
+}
+
+// this funktion will switch every LED short on an off in RGB colors
+
+void selftest_RGB(int pause){  
+  
+  
+  int i;
+  
+  for (i = 0; i <= 113; i = i + 1) { // wählt alle LEDS aus
+  leds[i] = CRGB::Red; 
+  }
+  FastLED.show();  // alle LEDS an
+  
+  delay(pause); // Pause
+  
+    for (i = 0; i <= 113; i = i + 1) { // wählt alle LEDS aus
+  leds[i] = CRGB::Green; 
+  }
+  FastLED.show();  // alle LEDS an
+  
+  delay(pause); // Pause
+  
+    for (i = 0; i <= 113; i = i + 1) { // wählt alle LEDS aus
+  leds[i] = CRGB::Blue; 
+  }
+  FastLED.show();  // alle LEDS an
+  
+  delay(pause); // Pause
+  
+  for (i = 0; i <= 114; i = i + 1) { // wählt alle LEDS aus
+  leds[i] = CRGB::Black;
+  }
+  FastLED.show();
 }
